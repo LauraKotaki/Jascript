cb9265dbc2a4854ec1233627da50f1a0128e4013
Laura Kotaki <62550500+LauraKotaki@users.noreply.github.com>
2020-03-27 11:38:57 -0300

    Adiciona index e exemplo de JS

---
+++ js/main.js
@@ -0,0 +1,12 @@
+//var nome = "Laura Kotaki";
+//var idade = 24;
+//var idade2= 25;
+//var frase = "Japão é o melhor do mundo";
+// alert("Bem vindo " + nome + " tem " + idade); //
+// alert (idade + idade2); //
+//console.log(nome); // ao inspecionar a pag, isto é carregado no Console
+//console.log(idade + idade2);
+//console.log(frase.replace("Japão","Brasil")); Replace =  irá substituir.
+//console.log(frase.toLowerCase()); toLowerCase = deixar tudo em minusculo
+// console.log(frase.toUpperCase()); // to UPPERcase = deixar tudo maiusculo
+
