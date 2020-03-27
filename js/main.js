975efeb2496aaba07b3268e48507d1379ba01e1d
Laura Kotaki <62550500+LauraKotaki@users.noreply.github.com>
2020-03-27 12:00:42 -0300

    Uso de Array e exemplos

--- js/main.js
+++ js/main.js
@@ -10,3 +10,30 @@
 //console.log(frase.toLowerCase()); toLowerCase = deixar tudo em minusculo
 // console.log(frase.toUpperCase()); // to UPPERcase = deixar tudo maiusculo
 
+//Exemplo array - List
+var lista = ["Maçã", "Pêra","Laranja"];
+lista.push("uva"); //adicionar um item a mais a lista
+lista.pop(); //retira o ultimo elemento da lista
+console.log(lista[1]);
+console.log(lista);
+console.log(lista.length); // ver o tamanho de itens na lista
+console.log(lista.reverse());
+
+console.log(lista);
+console.log(lista.toString()); //traz a lista com string
+
+console.log(lista[0]); //traz a maça pois é o primeiro elemento da lista
+console.log(lista.toString()[0]); //traz a letra M pois é a primeira letra da string
+
+console.log(lista.join(" -- ")); //traz a separação ao inves de virgula, o traço (ou o que determinar dentro do join)
+
+
+var fruta = {nome: "maça", cor: "vermelha"}
+console.log(fruta);
+console.log(fruta.nome);
+alert(fruta.cor);
+
+var frutas = [{nome: "maça", cor: "vermelha"}, {nome:"uva" , cor:"roxa"}]
+console.log(frutas);
+console.log(frutas[0].nome); //se não determinar a possição ficará undefined
+alert(frutas[1].cor);
